3781f11f6af08eeab217bcc02d265a71ace3b2c8
omkar rakshe <omkar rakshe@DESKTOP-IBA11JV>
2021-09-17 09:03:20 +0530

    my first commit

---
+++ Automation Qsp/src/Qsp/LaunchChromeBrowser.java
@@ -0,0 +1,13 @@
+package Qsp;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchChromeBrowser {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		new ChromeDriver();
+
+	}
+
+}
---
+++ Automation Qsp/src/Qsp/LaunchFirefoxBrowser.java
@@ -0,0 +1,13 @@
+package Qsp;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LaunchFirefoxBrowser {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver.exe");
+       
+        new FirefoxDriver();
+	}
+
+}
