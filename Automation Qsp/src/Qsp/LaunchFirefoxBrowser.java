def6f7349b68548ade8307d75dbafad35c2e6c46
omkarautomation2021 <90815692+omkarautomation2021@users.noreply.github.com>
2021-09-17 09:31:10 +0530

    Update LaunchFirefoxBrowser.java

--- Automation Qsp/src/Qsp/LaunchFirefoxBrowser.java
+++ Automation Qsp/src/Qsp/LaunchFirefoxBrowser.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class LaunchFirefoxBrowser {
 
 	public static void main(String[] args) {
+		System.out.println("omkar");
 		System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver.exe");
        
         new FirefoxDriver();
