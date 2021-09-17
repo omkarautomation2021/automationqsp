396db65b0dacec4a7cb282166c8b67be9b1f0e2d
omkarautomation2021 <90815692+omkarautomation2021@users.noreply.github.com>
2021-09-17 09:29:25 +0530

    Update LaunchChromeBrowser.java

--- Automation Qsp/src/Qsp/LaunchChromeBrowser.java
+++ Automation Qsp/src/Qsp/LaunchChromeBrowser.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class LaunchChromeBrowser {
 
 	public static void main(String[] args) {
+		System.out.println(" Omkar");
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 		new ChromeDriver();
 
